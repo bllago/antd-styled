59feb88fea9a4202b8797cdbd6c17c6a0971942d
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-08-10 22:31:06 +0300

    Row extended with flexbox and background props

--- src/components/LayoutSystem/Row/Row.js
+++ src/components/LayoutSystem/Row/Row.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Row as AntRow } from 'antd'
 import styled from 'styled-components'
-import { compose, layout, space } from 'styled-system'
+import { compose, layout, space, flexbox, background } from 'styled-system'
 import { useTheme } from '../../../hooks'
 
-const StyledAntRow = styled(AntRow)(compose(layout, space))
+const StyledAntRow = styled(AntRow)(compose(layout, flexbox, background, space))
 
 const Row = (props) => {
   const theme = useTheme()
