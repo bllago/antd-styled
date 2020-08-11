b16e8fb5b649e77192fe0008a6a5628043367129
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-08-11 12:51:16 +0300

    Updates for Row

--- src/components/LayoutSystem/Row/Row.js
+++ src/components/LayoutSystem/Row/Row.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { Row as AntRow } from 'antd'
 import styled from 'styled-components'
-import { compose, layout, space, flexbox, background } from 'styled-system'
+import {
+  compose,
+  layout,
+  space,
+  flexbox,
+  background,
+  color
+} from 'styled-system'
 import { useTheme } from '../../../hooks'
 
-const StyledAntRow = styled(AntRow)(compose(layout, flexbox, background, space))
+const StyledAntRow = styled(AntRow)(
+  compose(layout, flexbox, background, color, space)
+)
 
 const Row = (props) => {
   const theme = useTheme()
