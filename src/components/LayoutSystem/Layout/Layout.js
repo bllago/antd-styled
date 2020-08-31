56304aafdf5c4bd07aba5404547c5162b0dd6a00
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-08-31 23:13:23 +0300

    Extended layout component with color props

--- src/components/LayoutSystem/Layout/Layout.js
+++ src/components/LayoutSystem/Layout/Layout.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Layout as AntLayout } from 'antd'
 import styled from 'styled-components'
-import { compose, layout, background } from 'styled-system'
+import { compose, layout, background, color } from 'styled-system'
 
-const StyledAntLayout = styled(AntLayout)(compose(layout, background))
+const StyledAntLayout = styled(AntLayout)(compose(layout, background, color))
 
 const Layout = (props) => <StyledAntLayout {...props} />
 
