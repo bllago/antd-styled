4a95e33b10d113fa1a3d1c3a42cdbc9477bef226
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-09-01 00:41:57 +0300

    Extended Content component with flexbox props

--- src/components/LayoutSystem/Content/Content.js
+++ src/components/LayoutSystem/Content/Content.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { Layout } from 'antd'
 import styled from 'styled-components'
-import { compose, position, layout, space } from 'styled-system'
+import { compose, position, layout, space, flexbox } from 'styled-system'
 
 const StyledAntContent = styled(Layout.Content)(
-  compose(position, layout, space)
+  compose(position, layout, space, flexbox)
 )
 
 const Content = (props) => <StyledAntContent {...props} />
