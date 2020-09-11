97ff3bfaffc9efd385933c90f4e5d0b4ecbe4861
Evgeniy Bogdanov <bogdanov.jn@gmail.com>
2020-09-11 12:10:30 +0300

    Fix for skeleton border-radius

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -4,10 +4,18 @@ import styled from 'styled-components'
 
 const StyledAntSkeleton = styled(AntSkeleton)`
   .ant-skeleton-content .ant-skeleton-title {
-    border-radius: ${(props) => props.theme.borderRadius.sm};
+    border-radius: ${(props) =>
+      props &&
+      props.theme &&
+      props.theme.borderRadius &&
+      props.theme.borderRadius.sm};
   }
   .ant-skeleton-content .ant-skeleton-paragraph > li {
-    border-radius: ${(props) => props.theme.borderRadius.sm};
+    border-radius: ${(props) =>
+      props &&
+      props.theme &&
+      props.theme.borderRadius &&
+      props.theme.borderRadius.sm};
   }
 `
 
