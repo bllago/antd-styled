53ecb27938f4104f785f773c4d55f20a36d3b4e5
Maxim Makarov <maxim.makarov14@gmail.com>
2021-07-01 15:08:02 +0300

    update qonsoll package

--- src/components/Typography/Text/Text.js
+++ src/components/Typography/Text/Text.js
@@ -1,7 +1,7 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { Typography as AntTypography } from 'antd'
-import styled, { ThemeContext } from 'styled-components'
+import styled from 'styled-components'
 import {
   compose,
   space,
